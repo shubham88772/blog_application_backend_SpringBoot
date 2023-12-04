640bc3d3abc4244ae86ca483da906be1649a8a4e
shubham88772 <shubham88772@gmail.com>
2023-12-04 12:28:32 +0530

    Added Post Entity

---
+++ src/main/java/com/blog/project/entities/Post.java
@@ -0,0 +1,24 @@
+package com.blog.project.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "post_title",length = 100,nullable = false)
+    private String title;
+    @Column(length = 1000)
+    private String content;
+    private String imageName;
+    private Date addedDate;
+    @ManyToOne
+    private Category category;
+    @ManyToOne
+    private User user;
+
+}
--- src/main/java/com/blog/project/payloads/CategoryDto.java
+++ src/main/java/com/blog/project/payloads/CategoryDto.java
@@ -1,10 +1,17 @@
 package com.blog.project.payloads;
 
+import com.blog.project.entities.Post;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,4 +24,6 @@ public class CategoryDto {
     @NotBlank
     @Size(min = 10,message = "min is 10")
     private String categoryDescription;
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Post> posts=new ArrayList<>();
 }
